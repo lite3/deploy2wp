523d9592ee47178f5c52286d2ef0d0f691c8be7b
lite3 <lite3@qq.com>
2015-12-03 09:17:52 +0800

    fix can not check branch

--- scripts/deploy2wp.sh
+++ scripts/deploy2wp.sh
@@ -83,9 +83,9 @@ deploywpassets() {
     $SVN commit $SVN_AUTHORIZATION -m "auto deploy from git" .
 }
 
-if [[ "$TRAVIS_BRANCH"x == 'master' ]]; then
+if [[ "$TRAVIS_BRANCH"x == 'master'x ]]; then
     deploywptrunk
-elif [[ "$TRAVIS_BRANCH"x == 'assets' ]]; then
+elif [[ "$TRAVIS_BRANCH"x == 'assets'x ]]; then
     deploywpassets
 elif [[ "$(branchtype $TRAVIS_BRANCH)"x == 'tag'x ]]; then
     deploywptag
